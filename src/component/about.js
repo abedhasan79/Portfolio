96ea261257f05b8b822fb2df2d64de2f783b14ed
Abed Hasan <abedhasan79@gmail.com>
2022-11-15 00:15:40 -0500

    updated about page

--- src/component/about.js
+++ src/component/about.js
@@ -1,31 +1,37 @@
 import React from "react";
 import profileImg from './images/profile-pic.jpg'
 import './style.css'
-const styles ={
-    mainCard:{
-        display:'flex',
-        flexWrap:'wrap',
-        color:'white',
+const styles = {
+    mainCard: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        color: 'white',
     },
-    
-    img:{
-        height:'400px',
-        width:'400px',
-    }
+
+    img: {
+        height: '400px',
+        maxWidth: '400px',
+        borderRadius: '200px'
+    },
+    icon: {
+        fontSize: '48px',
+        marginRight: '2vh',
+    },
+
 };
 export default function about() {
     return (
-        <div  style={styles.mainCard}>
-            <div >
-                <div>
-                    <h2>About Me</h2>
-                </div>
+        <div className="about">
+            <div className="my-img">
+                <img src={profileImg} alt="Profile visual of creator" style={styles.img} />
+            </div>
 
-                <div >
-                    <img src={profileImg} alt="Profile visual of creator" style={styles.img}/>
+            <div >
+                <div className="text-center">
+                    <h2 className="aboutMeText">About Me</h2>
                 </div>
                 <div>
-                    <p>Full Stack Web Developer with a background in Applied
+                    <p className="about-p-tag">Full Stack Web Developer with a background in Applied
                         Mathematics and life-long dedication to learning.
                         Currently a student at University of Toronto coding
                         bootcamp. Gaining a vast array of knowledge in many
@@ -40,7 +46,12 @@ export default function about() {
                         project.
                     </p>
                 </div>
+                <div className="logo-git-lin">
+                    <a href="https://github.com/abedhasan79" className="logoColor"><i className="fa fa-github" style={styles.icon}></i></a>
+                    <a href="https://www.linkedin.com/in/abed-hasan-965497222/" className="logoColor"><i className="fa fa-linkedin-square" style={styles.icon}></i></a>
+                </div>
             </div>
+
         </div>
 
     );
