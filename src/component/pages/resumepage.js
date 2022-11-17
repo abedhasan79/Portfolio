fcc76b18725073d9c01ec6f425991c188a2300dc
Abed Hasan <abedhasan79@gmail.com>
2022-11-17 18:12:20 -0500

    updated resume page with animation

--- src/component/pages/resumepage.js
+++ src/component/pages/resumepage.js
@@ -2,6 +2,7 @@ import React from "react";
 import '../style.css';
 import { motion } from "framer-motion";
 import resumee from '../images/Abed Resume.pdf';
+
 const styles = {
 
     icon: {
@@ -21,13 +22,23 @@ export default function resume() {
             <div className="resumepage" >
                 <div className="resDown">
                     <h2 className="profText">Downlaod My Resume</h2>
-                    <a href={resumee} className="logoColor"><i className="fa fa-file" style={styles.icon}></i></a>
+                    <a href={resumee} className="logoColor">
+                        <motion.i className="fa fa-file" style={styles.icon}
+                            initial={{ x: 500 }}
+                            animate={{ x: 0 }}
+                            transition={{ type: 'spring' }}
+                        ></motion.i>
+                    </a>
                 </div>
 
                 <div className="proficiency">
                     <h2 className="profText">Front-End Proficiencies:</h2>
                     <div>
-                        <ul>
+                        <motion.ul
+                            initial={{ x: 500 }}
+                            animate={{ x: 0 }}
+                            transition={{ type: 'spring' }}
+                        >
                             <li>HTML</li>
                             <li>CSS</li>
                             <li>JavaScript</li>
@@ -35,13 +46,17 @@ export default function resume() {
                             <li>Responsive Design</li>
                             <li>React</li>
                             <li>Bootstrap</li>
-                        </ul>
+                        </motion.ul>
                     </div>
                 </div>
                 <div className="proficiency">
                     <h2 className="profText">Back-End Proficiencies:</h2>
                     <div >
-                        <ul>
+                        <motion.ul
+                            initial={{ x: 500 }}
+                            animate={{ x: 0 }}
+                            transition={{ type: 'spring' }}
+                        >
                             <li>APIs</li>
                             <li>Node</li>
                             <li>Express</li>
@@ -50,7 +65,7 @@ export default function resume() {
                             <li>REST</li>
                             <li>GraphQL</li>
                             <li>Mern State</li>
-                        </ul>
+                        </motion.ul>
                     </div>
                 </div>
             </div>
